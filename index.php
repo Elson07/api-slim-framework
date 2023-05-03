7dcac8f5f32a7ca837ce8af1ab0313f08f2ce6ff
Elson07 <elsonnunes07@gmail.com>
2023-05-03 12:59:23 -0300

    tipos de requesições

--- index.php
+++ index.php
@@ -1,63 +1,61 @@
 <?php
+//Interface que define o tipo da $request e $response, não é obrigatório, pois o slim ja faz isso, mas é uma boa pratica
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
 require 'vendor/autoload.php';
 
 $app = new \Slim\App;
 
-$app->get('/postagens2', function(){
-    echo 'Lista de postagens';
+// Padrão PSR7, é uma pratica de retorno ($response), melhor que usar echo!
+$app->get('/postagens', function(Request $request, Response $response){
+
+    //PSR7, getBody() é o corpo da mensagem, e write() escreve no corpo da mensagem.
+    $response->getBody()->write('Lista de postagens');
+
 });
 
-//Com o colchete [] o id é opcional no request
-$app->get('/usuarios[/{id}]', function($request, $response){
+//post cria dados no servidor (Insert)
+$app->post('/postagens/adiciona', function(Request $request, Response $response){
+
+    $post = $request->getParsedBody();
+    $nome = $post['nome'];
+    $email = $post['email'];
+
+    //Usando este mestodo, podemos savar os dados em um banco de dados usando o insert into, e no return poderiamos escrever uma menssagem de sucesso, ao inves dos dados, como fisemos agora.
+    //return $response->getBody()->write($nome . ' - ' . $email);
+
+    return $response->getBody()->write('Sucesso');
+
+});
+
+//put atualiza dados no servidor (Update), neste caso podemos passar um id como parâmetro, para saber qual dado sera atualizado. 
+$app->put('/postagens/atualizar', function(Request $request, Response $response){
+
+    $post  = $request->getParsedBody();
+    $id    = $post['id'];
+    $nome  = $post['nome'];
+    $email = $post['email'];
+
+    //Usando este mestodo, podemos atualizar os dados em um banco de dados usando o update, e no return poderiamos escrever uma menssagem de sucesso ao atualizar.
+
+    return $response->getBody()->write('Sucesso ao atualizar: ' . $id);
+
+});
+
+//delet deleta os dados no servidor (Delete), neste caso podemos passar um id como parâmetro, para saber qual dado sera atualizado. 
+$app->delete('/postagens/remove/{id}', function(Request $request, Response $response){
+
     $id = $request->getAttribute('id');
+    
+    //Usando este mestodo, podemos deletar os dados em um banco de dados usando o delete, e no return poderiamos escrever uma menssagem de sucesso ao deletar.
 
-    echo 'Lista de usuarios ou ID: ' . $id;
-});
-
-//Com colchete {} é possivel passar um paramentro para o resquest
-$app->get('/postagens[/{ano}[/{mes}]]', function($request, $response){
-    //Recupera o ano e mes passado como parametro pelo request
-    $ano = $request->getAttribute('ano');
-    $mes = $request->getAttribute('mes');
-
-    echo 'Lista de postagens: ' . $mes . '/' .$ano;
-});
-
-//.* significa que aceita quaquer tipo de parametro passado pelo request
-$app->get('/lista/{itens:.*}', function($request, $response){
-
-    $itens = $request->getAttribute('itens');
-    //echo $itens;                          
-    var_dump(explode("/", $itens));
-});
-
-//Nomear rotas 
-$app->get('/blog/postagens/{id}', function($request, $response){
-    echo "Listar postagem para um ID";
-//O setName define um nome para a rota '/blog/postagens/{id}' 
-})->setName('blog');
-
-$app->get('/meusite', function($request, $response){
-    //router é um metodo que recupera rota, e pathFor é o caminho que sera recuperado
-    $retorno = $this->get("router")->pathFor("blog", ["id"=> "10"]);
-
-    echo $retorno;
-});
-
-//Agrupar rotas
-//O metodo group é utulizado para agrupar rotas
-$app->group('/v1', function(){
-
-    $this->get('/usuarios', function(){
-        echo 'Lista de usuarios';
-    });
-
-    $this->get('/postagens', function(){
-        echo 'Lista de postagens';
-    });
+    return $response->getBody()->write('Sucesso ao deletar: ' . $id);
 
 });
-
-
 
 $app->run();
+
+
+
+
